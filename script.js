f32f5dcea22ff76024647ce00490eb9675228485
sophietrevisan <sophie.trevisan@escola.pr.gov.br>
2026-08-20 08:49:07 -0300

    ok

--- script.js
+++ script.js
@@ -4,7 +4,31 @@ document.addEventListener("DOMContentLoaded", () =>{
     if(!likeBtn)return;
     const likeSvg = likeBtn.querySelector("svg");
 
+
     //localiza o contador
 
-    let textNode = array
+    let textNode = array.from(likeBtn.childNodes).find(node) => mode.nodeType
+    === Node.TEXT_NODE && node.textContent.trim() !== ""
+);
+
+//zero o contador
+let cont = 0;
+
+//atualiza
+if(textNode);{
+     textNode.textContent = '0';
+}
+
+//coração
+function apllyLikeStyle(){
+likeSvg.style.fill = "#ef4444";
+likeSvg.style.stronk = "#ef4444";
+likeSvg.style.color = "#ef4444";
+
+
+//efeito cutida
+likeSvg.style.transform = "scale(1.3)";
+setTimeout(() => (likeSvg.style.transform = "scale(1)")150);
+
+
 })
